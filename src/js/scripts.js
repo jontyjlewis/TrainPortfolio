ab094318378ee6b6078593ebf5e8f413baa4e507
jontyjlewis <jontyjlewis@gmail.com>
2023-03-05 21:42:31 -0800

    cube spinning

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,6 +1,8 @@
 import * as THREE from 'three';
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js';
+// import * as dat from 'dat.gui';
 
-let scene, camera, renderer;
+let scene, camera, renderer, orbit;
 
 scene = new THREE.Scene();
 
@@ -16,7 +18,28 @@ camera = new THREE.PerspectiveCamera(
 );
 camera.position.set(-10, 30, 30);
 
+orbit = new OrbitControls(camera, renderer.domElement);
+orbit.update();
+
+// Floor Plane
+// const planeGeometry = new THREE.PlaneGeometry(30, 30);
+// const planeMaterial = new THREE.MeshStandardMaterial({
+//     color: 0xFFFFFF,
+//     side: THREE.DoubleSide
+// });
+// const plane = new THREE.Mesh(planeGeometry, planeMaterial);
+// scene.add(plane);
+// plane.rotation.x = -0.5 * Math.PI;
+
+const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
+const boxMaterial = new THREE.MeshBasicMaterial({color: 0x00FF00});
+const box = new THREE.Mesh(boxGeometry, boxMaterial);
+scene.add(box);
+
 function animate(time) {
+    box.rotation.x = time / 1000;
+    box.rotation.y = time / 1000;
+    
     renderer.render(scene, camera);
 }
 renderer.setAnimationLoop(animate);
